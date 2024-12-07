6dcddfceb44bec086086a7a703b65880aa84475a
mikhail2013ru <mikhail2013ru@gmail.com>
2024-12-07 17:10:51 +0300

    Initial commit

---
+++ src/js/modules/modals.js
@@ -0,0 +1,133 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll()
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                // console.log(e.target);
+                // e.target.classList.add('active-modal')
+                // modal.style.display = 'block'
+                // e.target.classList.contains('popup_calc_btn')
+                // if (!e.target.classList.contains('active-modal')) {
+                //     console.log(e.target.classList);
+                //     console.log(item);
+                //     console.log(e.target);
+                //     // e.preventDefault()
+                // } else {
+                //     // e.target.classList.add('active-modal')
+                // }
+                windows.forEach(item => {
+                    item.style.display = 'none'
+                })
+                
+                modal.style.display = 'block'
+                document.body.style.overflow = 'hidden'
+                document.body.style.marginRight = `${scroll}px`
+            })
+        })
+
+        close.addEventListener('click', (e) => {
+            console.log(e);
+            windows.forEach(item => {
+                item.style.display = 'none'
+            })
+
+            modal.style.display = 'none'
+            document.body.style.overflow = ''
+            document.body.style.marginRight = `0px`
+        })
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach(item => {
+                    console.log(item);
+                    item.style.display = 'none'
+                })
+
+                modal.style.display = 'none'
+                document.body.style.overflow = ''
+                document.body.style.marginRight = `0px`
+            }
+        })
+    }
+
+    function validateForm() {
+        const modalForm = document.querySelector('.popup_calc'),
+            inputs = modalForm.querySelectorAll('input[required]');
+        let isValid = true;
+        inputs.forEach(input => {
+          if (input.value.trim() === '') {
+            input.classList.add('invalid')
+            isValid = false;
+          } else {
+            input.value = ''
+            input.classList.remove('invalid')
+          }
+        });
+        
+        return isValid;
+        
+    }      
+      
+    // document.querySelector('.popup_calc_button').addEventListener('click', () => {
+    //     const modalForm = document.querySelector('.popup_calc'),
+    //         calcButton = document.querySelector('.popup_calc_button'),
+    //         calcProfile = document.querySelector('.popup_calc_profile');
+    //         let result = validateForm()
+    //         if (modalForm && result) {
+    //             calcButton.style.display = 'inline-block'
+    //             calcProfile.style.display = 'block'
+    //             modalForm.style.display = 'none'
+    //         } else {
+    //             alert('Пожалуйста, заполните все обязательные поля!')
+    //         }
+    // });
+
+    // document.querySelector('.popup_calc_profile_close').addEventListener('click', () => {
+    //     const calcProfile = document.querySelector('.popup_calc_profile');
+    //         calcProfile.style.display = 'none'
+    //         document.body.style.overflow = ''
+    // })
+
+    // return bindModal
+
+    function calcScroll() {
+        let div = document.createElement('div')
+        div.style.width = '50px'
+        div.style.height = '50px'
+        div.style.overflowY = 'scroll'
+        div.style.visibility = 'hidden'
+        document.body.appendChild(div)
+        let scrollWidth = div.offsetWidth - div.clientWidth
+        div.remove()
+
+        return scrollWidth
+    }
+
+    function showModalByTime(selector, time) {
+        setTimeout(function() {
+            let display;
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    display = 'block'
+                }
+            })
+
+            if (!display ) {
+                document.querySelector(selector).style.display = 'block'
+                document.body.style.overflow = 'hidden'
+            }
+            
+        }, time)
+    }
+
+    showModalByTime('.popup-consultation', 5000)
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close')
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close')
+}
+
+export default modals
