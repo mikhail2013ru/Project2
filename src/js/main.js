21d8500764fbc04081ed75693684db5115f2448b
mikhail2013ru <mikhail2013ru@gmail.com>
2025-01-10 15:20:57 +0300

    Плавный скролл

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import filter from './modules/filter'
 import pictureSize from './modules/pictureSize'
 import accordion from './modules/accordion'
 import burger from './modules/burger'
+import scrolling from './modules/scrolling'
 
 modals()
 sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn')
@@ -23,3 +24,4 @@ filter()
 pictureSize('.sizes-block')
 accordion('.accordion-heading', '.accordion-block')
 burger('.burger-menu', '.burger')
+scrolling('.pageup')
